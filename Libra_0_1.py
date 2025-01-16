ed1f3be4df06f47509e3df4a51fe9314ef49c5c6
Stefan Ivanov <Stefan_Ivanov94@live.com>
2025-01-16 15:56:18 +0200

    Added move command

--- Libra_0_1.py
+++ Libra_0_1.py
@@ -6,7 +6,8 @@ Created on Thu Dec 19 14:08:20 2024
 """
 
 books = {}      #TODO: Add file reader
-command = input("Do you want to: add, move, remove or search a book?")
+main_prompt = "Do you want to: add, move, remove, search or list all book? "
+command = input(main_prompt)
 
 while command != "end":  #TODO: Make list of STOP commands
     if command == "add":
@@ -23,26 +24,44 @@ while command != "end":  #TODO: Make list of STOP commands
             books[book] = [publisher,year,locations]
         print(f"A copy of {title} by {author} published by {publisher} in {year} was added in {location}.")
     elif command == "move":
-        pass
-    elif command == "remove":
         title = input("Book title:")
         author = input("Author name:")
-        location = input("Location to remove from:")
         book = title, author
         if book not in books:
             print("Book not found. Chech title and author spelling!")
-        elif location not in books[book][2]:
+            command = input(main_prompt)
+            continue
+        start_location = input("Location to move from:")
+        end_location = input("Location to move to:")
+        if start_location not in books[book][2]:
+            print("Book not found in specified location.")
+        else:
+            books[book][2].remove(start_location)
+            books[book][2].append(end_location)
+            print(f"Book succesfuly moved from {start_location} to {end_location}")
+    elif command == "remove":
+        title = input("Book title:")
+        author = input("Author name:")
+        book = title, author
+        if book not in books:
+            print("Book not found. Chech title and author spelling!")
+            command = input(main_prompt)
+            continue
+        location = input("Location to remove from:")
+        if location not in books[book][2]:
             print("Book not found in specified location.")
         else:
             books[book][2].remove(location)
             if len(books[book][2]) == 0:
                 del books[book]
+            print(f"{book[0]} by {book[1]} removed from {location}.")
     elif command == "search":
         print("If you want to search by author, leave title empty and vice versa.")
         title = input("Book title:")
         author = input("Author name:")
         if len(title) == 0 and len(author) == 0:
             print("You have to enter title and/or author!")
+            command = input(main_prompt)
             continue
         books_found = {}
         for book, info in books.items():
@@ -53,8 +72,8 @@ while command != "end":  #TODO: Make list of STOP commands
             elif book == (title,author):
                 books_found[book] = info
         for book, info in books_found.items():
-            print(f"{book[0]} by {book[1]} found in {info[2]}") 
+            print(f"{book[0]} by {book[1]} published by {info[0]} in {info[1]} found in {info[2]}") 
     else:
         print("Invalid command!")
         
-    command = input("Do you want to: add, move, remove or search a book?")
+    command = input(main_prompt)

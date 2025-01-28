1f5acb7cbb9ea0aae972153f95ecddbdef851c75
Stefan Ivanov <Stefan_Ivanov94@live.com>
2025-01-28 17:01:27 +0200

    Split list_all into simple and detailed

--- Libra_0_1.py
+++ Libra_0_1.py
@@ -7,6 +7,8 @@ Created on Thu Dec 19 14:08:20 2024
 
 books = {} 
 main_prompt = "Do you want to: add, move, remove, search or list all book? "
+not_found = "Book not found. Chech title and author spelling!"
+bad_lacation = "Book not found in specified location."
 
 with open('test_file_01.txt', 'r') as books_list:
     for raw_book in books_list:
@@ -38,13 +40,13 @@ while command != "end":  #TODO: Make list of STOP commands
         author = input("Author name:")
         book = title, author
         if book not in books:
-            print("Book not found. Chech title and author spelling!")
+            print(not_found)
             command = input(main_prompt)
             continue
         start_location = input("Location to move from:")
         end_location = input("Location to move to:")
         if start_location not in books[book][2]:
-            print("Book not found in specified location.")
+            print(bad_lacation)
         else:
             books[book][2].remove(start_location)
             books[book][2].append(end_location)
@@ -54,12 +56,12 @@ while command != "end":  #TODO: Make list of STOP commands
         author = input("Author name:")
         book = title, author
         if book not in books:
-            print("Book not found. Chech title and author spelling!")
+            print(not_found)
             command = input(main_prompt)
             continue
         location = input("Location to remove from:")
         if location not in books[book][2]:
-            print("Book not found in specified location.")
+            print(bad_lacation)
         else:
             books[book][2].remove(location)
             if len(books[book][2]) == 0:
@@ -83,11 +85,23 @@ while command != "end":  #TODO: Make list of STOP commands
                 books_found[book] = info
         for book, info in books_found.items():
             print(f"{book[0]} by {book[1]} published by {info[0]} in {info[1]} found in {'; '.join(info[2])}.") 
+        else:
+            print(not_found)
     elif command == "list all":
+        list_type = input("Simple(title, author and number of copies) or detailed list? ")
         number_of_books = 0
-        for book,info in books.items():
-            number_of_books += len(info[2])
-            print(f"{book[0]} by {book[1]} ({len(info[2])})") 
+        if list_type.lower() == "simple":
+            for book,info in books.items():
+                number_of_books += len(info[2])
+                print(f"{book[0]} by {book[1]} ({len(info[2])})") 
+        elif list_type.lower() == "detailed":
+            for book,info in books.items():
+                number_of_books += len(info[2])
+                print(f"{book[0]} by {book[1]} Publisher: {info[0]} Year: {info[1]} Found in: {', '.join(info[2])}") 
+        else:
+            print("Invalid command!")
+            command = input(main_prompt)
+            continue
         print(f"You have {number_of_books} books of wich {len(books)} are unique.")
     else:
         print("Invalid command!")

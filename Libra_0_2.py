be22280bad6c744a5b511b05823f6eb20fa1c880
Stefan Ivanov <Stefan_Ivanov94@live.com>
2025-02-26 11:15:24 +0200

    Improved the interface and separated file writing in a function

--- Libra_0_2.py
+++ Libra_0_2.py
@@ -8,7 +8,9 @@ Created on Thu Dec 19 14:08:20 2024
 #Dictionary to store books data
 books = {} 
 #commonly used prompts
-main_prompt = "Do you want to: add, move, remove, search or list all book? "
+main_prompt = "\n1.Add book\n2.Find book"
+main_prompt += "\n3.Move book\n4.Remove book\n5.Show all books\n6.Exit"
+main_prompt += "\n\nSelect command: "
 not_found = "Book not found. Chech title and author spelling!"
 bad_lacation = "Book not found in specified location."
 
@@ -24,6 +26,16 @@ def read_file(file_name:str)->dict:
             books[book_key] = book_info
         print(f"A total of {len(books)} unique books have been loaded")
     return books
+
+def write_file(file_name:str, books_data:str)->str:
+    saved_books = 0
+    with open(file_name,'w') as books_list:
+        for book, info in books_data.items():
+            book_as_list = [book[0], book[1], info[0], str(info[1]), '+'.join(info[2])]
+            book_as_string = '|'.join(book_as_list) + '\n'
+            books_list.write(book_as_string)
+            saved_books += 1
+    return f"A total of {saved_books} unique books have been saved in {file_name}"
 
 def read_book_key()->tuple:
     title = input("Book title:")
@@ -53,8 +65,7 @@ def add_book(book_key:tuple,book_location:str,books_data:dict)->dict:
         year = int(input("Year of release:"))
         locations = [book_location]
         books_data[book_key] = [publisher,year,locations]
-    print(f"A copy of {book_key[0]} by {book_key[1]} published by {publisher}\
-         in {year} was added in {book_location}.")
+    print(f"A copy of {book_key[0]} by {book_key[1]} published by {publisher} in {year} was added in {book_location}.")
     return books_data
 
 def move_book(book_key:tuple,books_data:dict,start_location:str,end_location:str)->dict:
@@ -77,17 +88,17 @@ def remove_book(book_key:tuple,books_data:dict,location:str)->dict:
     return books_data
 
 def print_all(books_data:dict):
-    list_type = input("Simple(title, author and number of copies) or detailed list? ")
+    list_type = input("1.Simple(title, author and number of copies)\n2.Detailed list\nChose view type: ")
     number_of_books = 0
-    if list_type != "simple" and list_type != "detailed":
-        print("Only 'simple' and 'detailed' commands accepted.")
+    if list_type not in ['1','2']:
+        print("Select option 1 or 2!")
         print_all(books_data)
     elif books_data:
         for book,info in books_data.items():
             number_of_books += len(info[2])
-            if list_type == "simple":
+            if list_type == "1":
                 print(f"{book[0]} by {book[1]} ({len(info[2])})") 
-            elif list_type == "detailed":
+            elif list_type == "2":
                 print(f"{book[0]} by {book[1]} Publisher: {info[0]} Year: {info[1]} Found in: {', '.join(info[2])}") 
         print(f"You have {number_of_books} books of wich {len(books_data)} are unique.")
     else:
@@ -100,41 +111,43 @@ def find_book(books_data:dict,book_to_find:tuple):
             books_found[book_key] = book_info
     print_all(books_found)
 
+print("Welcome to your library!\n")
 books = read_file('test_file_01.txt')
 command = input(main_prompt)
 
-while command != "end":  #TODO: Make list of STOP commands
-    if command == "add":
+while command != "6":
+    if command == "1":
         book = validate_title_and_author(read_book_key())
         location = input("Location:")
         books = add_book(book,location,books)
-    elif command == "move":
+
+    elif command == "2":
+        print("If you want to search by author, leave title empty and vice versa.")
+        find_book(books,read_book_key())
+    
+    elif command == "3":
         book = validate_title_and_author(read_book_key())
         if book in books:
             start_location = input("Location to move from:")
             end_location = input("Location to move to:")
             books = move_book(book,books,start_location,end_location)
         else:
-            print(not_found)       
-    elif command == "remove":
+            print(not_found)  
+
+    elif command == "4":
         book = validate_title_and_author(read_book_key())
         if book in books:
             location = input("Location to remove from:")
             books = remove_book(book,books,location)
         else:
             print(not_found)
-    elif command == "search":
-        print("If you want to search by author, leave title empty and vice versa.")
-        find_book(books,read_book_key())
-    elif command == "list all":
+
+    elif command == "5":
         print_all(books)
+
     else:
         print("Invalid command!")
         
     command = input(main_prompt)
 
-with open('test_file_01.txt','w') as books_list:
-    for book, info in books.items():
-        book_as_list = [book[0], book[1], info[0], str(info[1]), '+'.join(info[2])]
-        book_as_string = '|'.join(book_as_list) + '\n'
-        books_list.write(book_as_string)
+print(write_file('test_file_01.txt', books))
